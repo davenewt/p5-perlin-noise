aaba791e838256cd0e0f79ee861b83f56255a919
David Stroud <david@dwstroud.com>
2018-01-11 22:48:48 +0000

    v4
    Full window canvas, white on black, FPS output

--- particle.js
+++ particle.js
@@ -1,10 +1,13 @@
 function Particle() {
   this.pos = createVector(floor(random(width)), floor(random(height)));
-  this.vel = p5.Vector.random2D();
+  this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
+  this.maxspeed = 4;
+  this.prevPos = this.pos.copy();
 
   this.update = function() {
     this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
@@ -14,20 +17,44 @@ function Particle() {
   }
 
   this.show = function() {
-    stroke(255, 0, 0);
-    strokeWeight(4);
-    point(this.pos.x, this.pos.y);
+    colorMode(RGB, 255, 255, 255, 100);
+    stroke(255, 255, 255, 1);
+    strokeWeight(1);
+    // point(this.pos.x, this.pos.y);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
   }
 
   this.edges = function() {
-    if (this.pos.x > width)
+    if (this.pos.x > width) {
       this.pos.x = 0;
-    if (this.pos.x < 0)
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
       this.pos.x = width;
-    if (this.pos.y > height)
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
       this.pos.y = 0;
-    if (this.pos.y < 0)
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
       this.pos.y = height;
+      this.updatePrev();
+    }
+  }
+
+  this.follow = function(vectors) {
+    let x = floor(this.pos.x / scl);
+    let y = floor(this.pos.y / scl);
+    let index = x + y * cols; // taking 2D value into a 1D array
+    let force = vectors[index];
+    this.applyForce(force);
   }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,33 +1,37 @@
-let scl = 15;
+let scl = 10;
 let cols;
 let rows;
-let inc = 0.05; // increment value for perlin noise
+let inc = 0.02; // increment value for perlin noise
 let fr; // frame rate for display
 let zoff = 0;
 let particles = [];
+let flowfield;
 
 function setup() {
-  createCanvas(600, 400);
-  // createCanvas(windowWidth, windowHeight);
+  // createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight - 50);
   cols = floor(width / scl);
   rows = floor(height / scl);
   fr = createP('');
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 5000; i++) {
     particles[i] = new Particle();
   }
+  flowfield = new Array(cols * rows);
+  background(0);
 }
 
 function draw() {
-  background(255);
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
-      let index = (x + y * width) * 4;
-      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let index = x + y * cols;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
       let v = p5.Vector.fromAngle(angle);
+      v.setMag(0.1);
+      flowfield[index] = v;
       xoff += inc;
-    // stroke(200);
+    // stroke(155);
     // strokeWeight(1);
     // push();
     // translate(x * scl, y * scl);
@@ -37,13 +41,17 @@ function draw() {
     }
     yoff += inc;
   }
-  zoff += 0.005;
+  // zoff += 0.001;
 
   for (let i = 0; i < particles.length; i++) {
-    particles[i].show();
+    particles[i].follow(flowfield);
     particles[i].update();
     particles[i].edges();
+    particles[i].show();
   }
 
-  fr.html("Framerate: " + floor(frameRate()));
+  if (frameCount % 60 == 0) {
+    fr.html("FPS: " + floor(frameRate()));
+  }
+
 }
