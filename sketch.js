17dfbf24755f82ff5beac0cedf1e30fd0defbd27
David Stroud <david@dwstroud.com>
2018-01-11 20:47:50 +0000

    v2
    Grid of vectors from perlin noise, changing through time

--- sketch.js
+++ sketch.js
@@ -1,30 +1,38 @@
-let scl = 5;
+let scl = 15;
 let cols;
 let rows;
-let inc = 0.01;
+let inc = 0.05; // increment value for perlin noise
+let fr; // frame rate for display
+let zoff = 0;
 
 function setup() {
   createCanvas(600, 400);
   // createCanvas(windowWidth, windowHeight);
   cols = floor(width / scl);
   rows = floor(height / scl);
+  fr = createP('');
 }
 
 function draw() {
   background(0);
-
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
       let index = (x + y * width) * 4;
-      let r = noise(xoff, yoff) * 255;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle);
       xoff += inc;
-      fill(r);
-      stroke(150);
+      stroke(200);
       strokeWeight(1);
-      rect(x * scl, y * scl, scl, scl);
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0);
+      pop();
     }
     yoff += inc;
   }
+  zoff += 0.005;
+  fr.html("Framerate: " + floor(frameRate()));
 }
